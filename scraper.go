372c291b07647a3ceb9a362b75c63b8f810f7867
schlooben <admin@schlooben.xyz>
2022-03-18 19:59:30 +0000

    Add files via upload

---
+++ scraper.go
@@ -0,0 +1,19 @@
+package main
+
+
+	import (
+
+	"github.com/gocolly/colly"
+)
+
+func main(){
+ c := colly.NewCollector(colly.AllowedDomains("www.google.com"))
+
+ c.OnHTML("a[href]", func(h *colly.HTMLElement) {
+ fmt.Println(h.Text)
+ })
+
+c.Visit("www.google.com/")
+
+
+}
